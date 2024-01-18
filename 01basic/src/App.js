d04d8f88e314f4b8723ec33ec84225859c997fee
demo <ankesh28raj@gmail.com>
2024-01-18 17:47:08 +0530

    archives

---
+++ 01basic/src/App.js
@@ -0,0 +1,15 @@
+import Tea from './Tea'
+
+function App() {
+  return (
+    <>
+    <Tea />
+    <p>Evening tea time with biscuits</p>
+   <h1>Chai Aur React | Ankesh</h1>
+    </>
+  );
+}
+
+export default App;
+
+// UPPERCASE NAME FOR THE FUNCTION IS A MUST AND MANDATORY
